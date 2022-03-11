b049f3db5efa02aa7ffc0035e790779b7131ce03
Itcel Lerma <itcel2014@gmail.com>
2022-03-11 11:16:46 -0600

    adding the API link

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Main = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(
+        "https://strangers-things.herokuapp.com/api/2202-FTB-ET-WEB-FT/posts"
+      );
+    };
+  });
+
   return (
     <div>
       <h1>hello main</h1>
