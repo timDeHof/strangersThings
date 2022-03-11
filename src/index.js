a38c622adc1fe2eab6659d540cb8e8a54e378ba9
timDeHof <this.is.tim.dehof@gmail.com>
2022-03-11 11:29:13 -0500

    add Navbar.jsx

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Main } from "./components";
+import { Main, Navbar } from "./components";
 
 ReactDOM.render(
   <Router>
     <Main />
+    <Navbar />
   </Router>,
   document.getElementById("app")
 );
