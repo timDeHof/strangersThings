99e886e87affa9f306a505a5b479d3cfe1eed5ff
timDeHof <this.is.tim.dehof@gmail.com>
2022-03-15 10:31:08 -0400

    added login form to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Input from "./Input";
 
 function Login() {
-  return <h1>Please log in!</h1>;
+  return (
+    <div>
+      <h1>Please log in!</h1>
+      <form className="form">
+        <Input type="text" placeholder="username" />
+        <Input type="password" placeholder="password" />
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
