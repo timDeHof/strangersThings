c764812bbaabee414d91e7e84692af244d563bfc
timDeHof <this.is.tim.dehof@gmail.com>
2022-03-18 18:16:46 -0400

    removed edit post form to serpate component. will work on it tomorrow

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 // import forms from singlePost
 const Form = () => {
-  return;
+  return (
+    // <form
+    //     onSubmit={(e) => {
+    //       handleSubmit(e, post._id);
+    //     }}
+    //   >
+    //     <input
+    //       placeholder="title"
+    //       value={newTitle}
+    //       onChange={(e) => setNewTitle(e.target.value)}
+    //     />
+    //     <button type="submit">Update </button>
+    //   </form>
+    // {/* Description */}
+    // <p className="description">{`Description: ${post.description}`}</p>
+    // <form onSubmit={handleSubmit}>
+    //   <input
+    //     placeholder="description"
+    //     value={newDescription}
+    //     onChange={(e) => setNewDescription(e.target.value)}
+    //   />
+    //   <button type="submit">Update </button>
+    // </form>
+    //}
+    // <p className="price">{`Price: $ ${post.price}`}</p>
+    // <form onSubmit={handleSubmit}>
+    //   <input
+    //     placeholder="$ price"
+    //     value={newPrice}
+    //     onChange={(e) => setNewPrice(e.target.value)}
+    //   />
+    //   <button type="submit">Update</button>
+    // </form>
+
+    {
+      /* we need a section for will deliver */
+    }
+    //</div>
+  );
 };
 
 export default Form;
--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -33,42 +33,16 @@ const SinglePost = ({ post, token }) => {
     <div className="post" key={post._id}>
       {/* Title */}
       <h2>Title: {post.title}</h2>
-      <form
-        onSubmit={(e) => {
-          handleSubmit(e, post._id);
-        }}
-      >
-        <input
-          placeholder="title"
-          value={newTitle}
-          onChange={(e) => setNewTitle(e.target.value)}
-        />
-        <button type="submit">Update </button>
-      </form>
+
       {/* Description */}
       <p className="description">{`Description: ${post.description}`}</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="description"
-          value={newDescription}
-          onChange={(e) => setNewDescription(e.target.value)}
-        />
-        <button type="submit">Update </button>
-      </form>
+
       {/* Author */}
       <p className="author">
         <strong>{`Author: ${post.author.username}`}</strong>
       </p>
       {/* Price */}
       <p className="price">{`Price: $ ${post.price}`}</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="$ price"
-          value={newPrice}
-          onChange={(e) => setNewPrice(e.target.value)}
-        />
-        <button type="submit">Update</button>
-      </form>
 
       {/* we need a section for will deliver */}
     </div>
