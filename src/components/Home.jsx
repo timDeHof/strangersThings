4e381fdf987db64ad75ec67e01f17bab895afb5c
Itcel Lerma <itcel2014@gmail.com>
2022-03-19 17:57:25 -0500

    added text to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <h1> Hello!</h1>;
+  return (
+    <div className="homePage">
+      <h1>
+        HEY THERE STRANGER! <br /> Welcome to STRANGER THING'S
+      </h1>
+      <p>The most popular place where Stranger's buy/sell Thing's!</p>
+      <p>
+        Please log in to show the things you have to offer or you can just check
+        out the post from other strangers!
+      </p>
+    </div>
+  );
 };
 export default Home;
