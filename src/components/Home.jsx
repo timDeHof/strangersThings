5e1b2a8ca56d3fa5c764354d4372ff0de9015a34
Itcel Lerma <itcel2014@gmail.com>
2022-03-19 18:12:31 -0500

    added links to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,17 @@ const Home = () => {
   return (
     <div className="homePage">
       <h1>
-        HEY THERE STRANGER! <br /> Welcome to STRANGER THING'S
+        HEY THERE STRANGER! <br />
+        <br /> Welcome to STRANGER THING'S
       </h1>
+      <br />
+      <br />
       <p>The most popular place where Stranger's buy/sell Thing's!</p>
       <p>
-        Please log in to show the things you have to offer or you can just check
-        out the post from other strangers!
+        Please
+        <a href="http://localhost:3000/LogInForm">Log In</a>to show the things
+        you have to offer or you can just check out the
+        <a href="http://localhost:3000/postsList">Post</a> from other strangers!
       </p>
     </div>
   );
