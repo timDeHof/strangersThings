33dc0db7367c02f41ae7a7d1ac4aa7301442a1f6
Itcel Lerma <itcel2014@gmail.com>
2022-03-20 15:33:30 -0500

    css on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,9 +11,8 @@ const Home = () => {
       <br />
       <p>The most popular place where Stranger's buy/sell Thing's!</p>
       <p>
-        Please
-        <a href="http://localhost:3000/LogInForm">Log In</a>to show the things
-        you have to offer or you can just check out the
+        Please <a href="http://localhost:3000/LogInForm">Log In</a> to show the
+        things you have to offer or you can just check out the{" "}
         <a href="http://localhost:3000/postsList">Post</a> from other strangers!
       </p>
     </div>
